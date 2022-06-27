ccfc0b6ffc90f0ee479dc6d3684a445c3d3743a9
Sanith S <83565729+Sanith643@users.noreply.github.com>
2022-06-27 12:32:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('.｡.✿ᴊᴏɪɴ ᴍʏ ɢʀᴏᴜᴘ ᴛᴏ ᴜꜱᴇ ᴍᴇ.｡.✿', url='http://t.me/seriesloversKerala')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
