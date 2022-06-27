6b6c4adadfa58536009a2b760745107cd09ca1c5
Sanith S <83565729+Sanith643@users.noreply.github.com>
2022-06-27 12:47:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -404,13 +404,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-        ], [
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
-        ], [
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('.｡.✿ ᴊᴏɪɴ ᴍʏ ɢʀᴏᴜᴘ ᴛᴏ ᴜꜱᴇ ᴍᴇ ✿.｡.', url='http://t.me/seriesloversKerala')
+            ],[
+            InlineKeyboardButton('.｡.✿ ꜱᴇᴀʀᴄʜ', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('ᴄʀᴇᴀᴛᴏʀ ✿.｡.', url='https://t.me/Rahulthrilok')
+            ],[
+            InlineKeyboardButton('.｡.✿ ʜᴇʟᴘ', callback_data='help'),
+            InlineKeyboardButton('ᴀʙᴏᴜᴛ ✿.｡.', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
