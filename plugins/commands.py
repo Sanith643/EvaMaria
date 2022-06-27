b52e84e6cb261266c754ca69ea591bbb908de93c
Sanith S <83565729+Sanith643@users.noreply.github.com>
2022-06-27 12:40:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,13 +42,13 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('.｡.✿ᴊᴏɪɴ ᴍʏ ɢʀᴏᴜᴘ ᴛᴏ ᴜꜱᴇ ᴍᴇ.｡.✿', url='http://t.me/seriesloversKerala')
+            InlineKeyboardButton('.｡.✿ ᴊᴏɪɴ ᴍʏ ɢʀᴏᴜᴘ ᴛᴏ ᴜꜱᴇ ᴍᴇ ✿.｡.', url='http://t.me/seriesloversKerala')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('.｡.✿ ꜱᴇᴀʀᴄʜ', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('ᴄʀᴇᴀᴛᴏʀ ✿.｡.', url='https://t.me/Rahulthrilok')
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('.｡.✿ ʜᴇʟᴘ', callback_data='help'),
+            InlineKeyboardButton('ᴀʙᴏᴜᴛ ✿.｡.', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
